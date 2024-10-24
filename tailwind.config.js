96a2d78514ed37dc0d8ebcf01f0874abd307093f
Alexander <alexanderchrist203@gmail.comhttp://127.0.0.1:3002/index.html>
2024-10-24 11:16:02 +0100

    Clearing files for new project

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ export default {
     "./index.html", "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    
+    extend: {
+      colors: {
+        "twitter-blue" : "#1da1f2"
+      },
+    },
   },
   plugins: [],
 }
